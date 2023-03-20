c20406d59dabbc27ab09a44143e88078c57741f8
COBI-98 <tkdgus968@naver.com>
2023-03-20 20:52:09 +0900

    feat: fieldError ObjectError

--- springMVC2-4/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ springMVC2-4/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -48,7 +48,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         // 검증 로직
@@ -74,6 +74,78 @@ public class ValidationItemControllerV2 {
         if(bindingResult.hasErrors()){
             log.info("errors = {}",bindingResult);
 //            model.addAttribute("errors",bindingResult); 생략가능
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())){
+            // 바인딩실패했나 false
+            bindingResult.addError(new FieldError("item","itemName",item.getItemName(),false,null,null,"상품 이름은 필수입니다."));
+        }
+        if(item.getPrice() == null || item.getPrice() < 1_000 || item.getPrice() > 1_000_000){
+            bindingResult.addError(new FieldError("item","price",item.getPrice(),false,null,null,"가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+        }
+        if(item.getQuantity() == null || item.getQuantity() >= 9_999){
+            bindingResult.addError(new FieldError("item","quantity",item.getQuantity(),false,null,null,"수량은 최대 9,999개 까지 허용합니다."));
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10_000){
+                bindingResult.addError(new ObjectError("item",null,null,"가격 * 수량의 합은 10,000이상 이어야합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}",bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())){
+            // 바인딩실패했나 false
+            bindingResult.addError(new FieldError("item","itemName",item.getItemName(),false,new String[]{"required.item.itemName"},null,null));
+        }
+        if(item.getPrice() == null || item.getPrice() < 1_000 || item.getPrice() > 1_000_000){
+            bindingResult.addError(new FieldError("item","price",item.getPrice(),false,new String[]{"range.item.price"},new Object[]{1_000,1_000_000},null));
+        }
+        if(item.getQuantity() == null || item.getQuantity() >= 9_999){
+            bindingResult.addError(new FieldError("item","quantity",item.getQuantity(),false,new String[]{"max.item.quantity"},new Object[]{9_999},null));
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10_000){
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"},new Object[]{10_000,resultPrice},null));
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}",bindingResult);
             return "validation/v2/addForm";
         }
 
