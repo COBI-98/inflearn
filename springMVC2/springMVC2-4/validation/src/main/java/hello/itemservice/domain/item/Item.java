b95d3b973781eba70284cf1542d8fa47b6551b28
COBI-98 <tkdgus968@naver.com>
2023-03-21 02:32:09 +0900

    feat: validationv3 bean Validation

--- springMVC2/springMVC2-4/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ springMVC2/springMVC2-4/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,8 +5,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원을 넘게 해주세요")
 public class Item {
 
     private Long id;
