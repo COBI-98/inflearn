c884557cd14b676d0577f3cd9fbfcd49f0eb8f4f
COBI-98 <tkdgus968@naver.com>
2023-04-07 03:22:00 +0900

    feat: 주문 도메인 개발

--- springJPA1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ springJPA1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,13 @@
 package jpabook.jpashop.service;
 
+import java.time.LocalDateTime;
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.DeliveryStatus;
 import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
