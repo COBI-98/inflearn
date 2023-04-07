a22ad012604ccb029885872e082ca466e524bdd4
COBI-98 <tkdgus968@naver.com>
2023-04-08 02:44:27 +0900

    feat: 조회용 샘플데이터 추가

--- springJPA1/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ springJPA1/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -22,11 +22,26 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    /**
+     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
+     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를
+     위한 모든 요청 요구사항을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * 결론
+     * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     */
+    //조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, api가 이거하나인가! 화면에 종속적이지 마라!
     @GetMapping("/api/v1/members") // Arrays 확장성 x
     public List<Member> membersV1(){
         return memberService.findMembers(); // 모든 정보들이 노출
     }
 
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     */
     @GetMapping("/api/v2/members")
     public Result membersV2(){
         List<Member> findMembers = memberService.findMembers();
