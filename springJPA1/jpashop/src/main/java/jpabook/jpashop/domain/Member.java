ee2b02b8579dbdda6391d885c8eaca6c6dde172e
COBI-98 <tkdgus968@naver.com>
2023-04-08 02:12:59 +0900

    feat: 회원 API 개발

--- springJPA1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ springJPA1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -8,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,11 +21,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+//    @NotEmpty 별도의 dto를 활용하여 설정해주는것이 정석
     private String name;
 
     @Embedded
     private Address address;
 
+//    @JsonIgnore api 조회에서 제외시키기
     @OneToMany(mappedBy = "member") // 읽기전용
     private List<Order> orders = new ArrayList<>(); // 컬렉션은 필드에서 초기화하자. 하이버네이트 내부 매커니즘 문제를발생시킬수있음 바꾸지 말기
 
