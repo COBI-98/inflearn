4eab70ce6b8a48b109e801f5a024234032e18230
COBI-98 <tkdgus968@naver.com>
2023-04-22 04:41:34 +0900

    test: 인텔리제이 단축키를 이용한 import문 제거, 코드 서식수정

--- springJPA1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ springJPA1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,7 +6,6 @@ import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
@@ -17,7 +16,6 @@ import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
-import jpabook.jpashop.service.query.OrderDto;
 import jpabook.jpashop.service.query.OrderQueryService;
 import lombok.Data;
 import lombok.Getter;
@@ -30,110 +28,125 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class OrderApiController {
 
-    private final OrderRepository orderRepository;
-    private final OrderQueryRepository orderQueryRepository;
+  private final OrderRepository orderRepository;
+  private final OrderQueryRepository orderQueryRepository;
 
-    private final OrderQueryService orderQueryService;
+  private final OrderQueryService orderQueryService;
 
-    @GetMapping("/api/v1/orders")
-    public List<Order> ordersV1() {
-        List<Order> all = orderRepository.findAllByString(new OrderSearch());
-        for (Order order : all) {
-            order.getMember().getName();
-            order.getDelivery().getAddress();
+  @GetMapping("/api/v1/orders")
+  public List<Order> ordersV1() {
+    List<Order> all = orderRepository.findAllByString(new OrderSearch());
+    for (Order order : all) {
+      order.getMember().getName();
+      order.getDelivery().getAddress();
 
-            List<OrderItem> orderItems = order.getOrderItems();
-            orderItems.forEach(orderItem -> orderItem.getItem().getName());
-        }
-        return all;
+      List<OrderItem> orderItems = order.getOrderItems();
+      orderItems.forEach(orderItem -> orderItem.getItem().getName());
     }
+    return all;
+  }
 
-    @GetMapping("/api/v2/orders")
-    public List<OrderDto> ordersV2() {
-        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
-        List<OrderDto> result = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(toList());
+  @GetMapping("/api/v2/orders")
+  public List<OrderDto> ordersV2() {
+    List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+    List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(toList());
 
-        return result;
+    return result;
+  }
+
+  @GetMapping("/api/v3/orders")
+  public List<jpabook.jpashop.service.query.OrderDto> ordersV3() {
+    return orderQueryService.ordersV3();
+  }
+
+  @GetMapping("/api/v3.1/orders")
+  public List<OrderDto> ordersV3_page(
+      @RequestParam(value = "offset", defaultValue = "0") int offset,
+      @RequestParam(value = "limit", defaultValue = "100") int limit) {
+    List<Order> orders =
+        orderRepository.findAllWithMemberDelivery(offset, limit); // 페이징 영향 x -> ToOne fetch join
+
+    List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(toList());
+
+    return result;
+  }
+
+  @GetMapping("/api/v4/orders")
+  public List<OrderQueryDto> ordersV4() {
+    return orderQueryRepository.findOrderQueryDtos();
+  }
+
+  @GetMapping("/api/v5/orders")
+  public List<OrderQueryDto> ordersV5() {
+    return orderQueryRepository.findAllByDto_optimization();
+  }
+
+  @GetMapping("/api/v6/orders")
+  public List<OrderQueryDto> ordersV6() {
+    List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+    return flats.stream()
+        .collect(
+            groupingBy(
+                o ->
+                    new OrderQueryDto(
+                        o.getOrderId(),
+                        o.getName(),
+                        o.getOrderDate(),
+                        o.getOrderStatus(),
+                        o.getAddress()),
+                mapping(
+                    o ->
+                        new OrderItemQueryDto(
+                            o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()),
+                    toList())))
+        .entrySet()
+        .stream()
+        .map(
+            e ->
+                new OrderQueryDto(
+                    e.getKey().getOrderId(),
+                    e.getKey().getName(),
+                    e.getKey().getOrderDate(),
+                    e.getKey().getOrderStatus(),
+                    e.getKey().getAddress(),
+                    e.getValue()))
+        .collect(toList());
+  }
+
+  @Data
+  static class OrderDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemDto> orderItems; // orderItem 도 dto로 수정해야한다.
+
+    public OrderDto(Order order) {
+      orderId = order.getId();
+      name = order.getMember().getName(); // LAZY 초기화
+      orderDate = order.getOrderDate();
+      orderStatus = order.getStatus();
+      address = order.getDelivery().getAddress(); // LAZY 초기화
+      orderItems =
+          order.getOrderItems().stream()
+              .map(orderItem -> new OrderItemDto(orderItem))
+              .collect(toList());
     }
+  }
 
-    @GetMapping("/api/v3/orders")
-    public List<jpabook.jpashop.service.query.OrderDto> ordersV3() {
-        return orderQueryService.ordersV3();
+  @Getter
+  static class OrderItemDto {
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderItemDto(OrderItem orderItem) {
+      itemName = orderItem.getItem().getName();
+      orderPrice = orderItem.getOrderPrice();
+      count = orderItem.getCount();
     }
-
-    @GetMapping("/api/v3.1/orders")
-    public List<OrderDto> ordersV3_page(
-            @RequestParam(value = "offset", defaultValue = "0") int offset,
-            @RequestParam(value = "limit", defaultValue = "100") int limit) {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit); //페이징 영향 x -> ToOne fetch join
-
-        List<OrderDto> result = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(toList());
-
-        return result;
-    }
-
-    @GetMapping("/api/v4/orders")
-    public List<OrderQueryDto> ordersV4() {
-        return orderQueryRepository.findOrderQueryDtos();
-    }
-
-    @GetMapping("/api/v5/orders")
-    public List<OrderQueryDto> ordersV5() {
-        return orderQueryRepository.findAllByDto_optimization();
-    }
-
-    @GetMapping("/api/v6/orders")
-    public List<OrderQueryDto> ordersV6() {
-        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
-
-        return flats.stream()
-                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
-                                o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
-                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
-                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
-                )).entrySet().stream()
-                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
-                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
-                        e.getKey().getAddress(), e.getValue()))
-                .collect(toList());
-    }
-
-    @Data
-    static class OrderDto {
-
-        private Long orderId;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
-        private List<OrderItemDto> orderItems; //orderItem 도 dto로 수정해야한다.
-
-        public OrderDto(Order order) {
-            orderId = order.getId();
-            name = order.getMember().getName(); //LAZY 초기화
-            orderDate = order.getOrderDate();
-            orderStatus = order.getStatus();
-            address = order.getDelivery().getAddress(); //LAZY 초기화
-            orderItems = order.getOrderItems().stream()
-                    .map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(toList());
-        }
-    }
-
-    @Getter
-    static class OrderItemDto {
-        private String itemName;
-        private int orderPrice;
-        private int count;
-
-        public OrderItemDto(OrderItem orderItem) {
-            itemName = orderItem.getItem().getName();
-            orderPrice = orderItem.getOrderPrice();
-            count = orderItem.getCount();
-        }
-    }
+  }
 }
