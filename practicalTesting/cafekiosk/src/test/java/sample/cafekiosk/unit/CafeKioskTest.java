32db9c4ac3e07b3cfbeaca9bf89ea81494634fb4
COBI-98 <tkdgus968@naver.com>
2023-06-23 17:56:01 +0900

    feat: 추가 요구사항 구현

--- practicalTesting/cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ practicalTesting/cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,9 +1,6 @@
 package sample.cafekiosk.unit;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
@@ -24,6 +21,36 @@ class CafeKioskTest {
 //        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @DisplayName("여러잔 주문")
+    @Test
+    public void addSeveralBeverages() throws Exception{
+        //given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        //when
+        cafeKiosk.add(americano, 2);
+
+        //then
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    @DisplayName("0잔 주문")
+    @Test
+    public void addZero() throws Exception{
+        //given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> cafeKiosk.add(americano,0))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("음료는 1잔 이상 주문하셔야합니다.");
     }
 
     @DisplayName("삭제")
