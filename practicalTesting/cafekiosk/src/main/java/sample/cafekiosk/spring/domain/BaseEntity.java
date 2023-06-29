ad060b6a5685209b18457ced54597feb63f8c54a
COBI-98 <tkdgus968@naver.com>
2023-06-29 15:21:18 +0900

    fix: 데이터 생성, 감시 리스너 선언 오류

--- practicalTesting/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/BaseEntity.java
+++ practicalTesting/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/BaseEntity.java
@@ -6,10 +6,11 @@ import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AutoCloseable.class)
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity { // 인스턴스 필요 없 -> 추상
 
     @CreatedDate
